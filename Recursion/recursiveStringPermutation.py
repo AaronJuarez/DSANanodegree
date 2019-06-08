f0cf0de814ab8ba69755157bd8074897e65fd82f
Aaron Juarez <aarjuamtz@gmail.com>
2019-06-08 13:34:08 -0400

    Recursive String Permutation

---
+++ Recursion/recursiveStringPermutation.py
@@ -0,0 +1,46 @@
+def permutations(string):
+    """
+    :param: input string
+    Return - list of all permutations of the input string
+    TODO: complete this function to return a list of all permutations of the string
+    """
+    perm = []
+    if len(string) == 0:
+        perm.append('')
+    else:
+        firstChar = string[:1]
+        sub_perm = permutations(string[1:])
+        for p in sub_perm:
+            for j in range(len(p)+1):
+                temp = p
+                new_string = temp[:j] + firstChar + temp[j:]
+                perm.append(new_string)
+    return perm
+
+def test_function(test_case):
+    string = test_case[0]
+    solution = test_case[1]
+    output = permutations(string)
+    
+    output.sort()
+    solution.sort()
+    
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+string = 'ab'
+solution = ['ab', 'ba']
+test_case = [string, solution]
+test_function(test_case)
+
+string = 'abc'
+output = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
+test_case = [string, output]
+test_function(test_case)
+
+string = 'abcd'
+output = ['abcd', 'bacd', 'bcad', 'bcda', 'acbd', 'cabd', 'cbad', 'cbda', 'acdb', 'cadb', 'cdab', 'cdba', 'abdc', 'badc', 'bdac', 'bdca', 'adbc', 'dabc', 'dbac', 'dbca', 'adcb', 'dacb', 'dcab', 'dcba']
+test_case = [string, output]
+test_function(test_case)
